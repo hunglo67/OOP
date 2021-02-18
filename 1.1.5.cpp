0e7eb160a77d7cebff7c7adafcd89b09156a381d
hunglo67 <71253293+hunglo67@users.noreply.github.com>
2021-02-18 19:54:34 +0700

    Add project files.

---
+++ 1.1.5.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+
+	int n;
+	cin >> n;
+	if (n < 1 || n>9)
+	{
+		cout << " n is wrong";
+	}
+	else
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				if (j == i)
+					cout << j;
+				else
+					cout << j << " ";
+			}
+			if (i != n)
+				cout << endl;
+		}
+	}
+	return (0);
+}
