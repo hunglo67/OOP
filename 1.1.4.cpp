b60f42ee98123828dfd0f7e0b1199a88c409ad22
hunglo67 <71253293+hunglo67@users.noreply.github.com>
2021-02-18 19:58:57 +0700

    Add files via upload

---
+++ 1.1.4.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int x;       
+	cin>>x;       
+	if(x%2==0)               
+		cout<<"even";        
+	else                
+		cout<<"uneven";        
+	return(0);
+}
