352f1dfda3be991560a223a52290cea097486bb4
hunglo67 <71253293+hunglo67@users.noreply.github.com>
2021-03-02 16:41:31 +0700

    Add files via upload

---
+++ 3.2.4.1(v).cpp
@@ -0,0 +1,27 @@
+﻿
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class A
+{
+public:
+    A()
+
+    {
+        cout << "Constructor \n";
+    }
+
+    // destructor
+    ~A()
+    {
+        cout << "Destructor\n";
+    }
+};
+
+int main()
+{
+    {
+        A() ;   // Constructor Called
+    }
+return 0;
+}
